e84aafe205f46913d8f3fad4cd4afe27e677119e
Dan <dan.weatherill@cantab.net>
2023-09-16 15:02:22 +0100

    VTK at least now basically builds

--- recipes/vtk/all/conanfile.py
+++ recipes/vtk/all/conanfile.py
@@ -1,6 +1,7 @@
 from conan import ConanFile
 from conan.tools.files import get, copy, apply_conandata_patches, export_conandata_patches
 from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain, CMakeDeps
+import pathlib
 
 class VTKConan(ConanFile):
     name = "vtk"
@@ -23,25 +24,20 @@ class VTKConan(ConanFile):
 
 
     def requirements(self):
-
-        #in conan center
-        self.requires("utf8.h/cci.20210310")
-        self.requires("fast_float/3.11.0")
+        self.requires("fast_float/[>=3.9 <4]")
         self.requires("exprtk/0.0.2")
         self.requires("pugixml/1.13")
-        self.requires("libpng/1.6.40")
+        self.requires("libpng/[>=1.6.37]")
         self.requires("freetype/2.13.0")
         self.requires("libtiff/4.5.1") #NOTE: forced by proj
-        self.requires("eigen/3.4.0")
+        self.requires("eigen/[^3.3.9]")
         self.requires("double-conversion/3.3.0")
         self.requires("lz4/1.9.4")
         self.requires("glew/2.2.0")
-        self.requires("hdf5/1.14.0") #NOTE: forced by current netCDF dependency
         self.requires("ogg/1.3.5")
         self.requires("theora/1.1.1")
         self.requires("netcdf/4.8.1")
         self.requires("nlohmann_json/3.11.2")
-        self.requires("sqlite3/3.43.1", run=True, build=True)
         self.requires("proj/9.2.1")
         self.requires("jsoncpp/1.9.5")
         self.requires("libxml2/2.11.4")
@@ -52,12 +48,17 @@ class VTKConan(ConanFile):
 
         #NOTE: the current proj and netCDF recipes conflict with each other.
         #it appears to be cheaper to rebuild netCDF than proj, so we choose the
-        #libcurl that proj wants (which is newer anyway) and compatibility should be
-        #fine with netCDF
-        self.requires("libcurl/8.2.1", override=True) #NOTE: solve conflict between proj and netCDF
+        #libcurl and sqlite3 that proj wants (which is newer anyway) and compatibility should be
+        #fine with netCDF rebuild. Given that we are going to have to rebuild netCDF anyway,
+        #then we might as well bump the HDF5 version that it is otherwise limiting
+        self.requires("libcurl/8.2.1", override=True)
 
-        #NOT in conan center
-        # Verdict
+
+        #sqlite3 binary required to build, but also required to link into module
+        sqlitestr: str = "sqlite3/3.42.0"
+        self.requires(sqlitestr, build=True, run=True)
+        self.requires(sqlitestr, headers=True, libs=True)
+        self.requires("hdf5/1.14.1", force=True) #NOTE: current netCDF dependency uses 1.14.0
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -65,11 +66,15 @@ class VTKConan(ConanFile):
         tc.cache_variables["VTK_LEGACY_REMOVE"] = True
 
 
-        #externals that aren't found and appear to have no conan packages at the moment
+        #deps that aren't found and appear to have no conan packages at the moment
         internal_mods = ["verdict", "pegtl", "ioss", "gl2ps"]
+
+        #deps that are in conan center but of inappropriate versions (need recipe updates)
+        internal_mods += ["utf8"] # nearest is utf8.h/cci.20210310
+        
         for mod in internal_mods:
             tc.cache_variables[f"VTK_MODULE_USE_EXTERNAL_VTK_{mod}"] = False
-        tc.generate()
+
 
 
         deps = CMakeDeps(self)
@@ -80,11 +85,25 @@ class VTKConan(ConanFile):
         deps.set_property("eigen", "cmake_target_name", "Eigen3::Eigen3")
         deps.set_property("lz4", "cmake_target_name", "LZ4::LZ4")
 
+
+        #NOTE: there is an incompatibility between the include directory base in the
+        #double-conversion conan recipe and the CMake find module
+        #used by VTK when doubhle-conversion is in external mode. One way round this
+        #is to allow use of VTK's internal find module for double-conversion, by correctly setting
+        #double-conversion_ROOT
+        deps.set_property("double-conversion", "cmake_find_mode", "none")
+        dcincl = self.dependencies["double-conversion"].cpp_info.includedirs[0]
+        dcroot = str(pathlib.Path(dcincl).parents[0])
+        self.output.info(f"dc_include_dir: {dcincl}")
+        self.output.info(f"dc_root_dir: {dcroot}")
+        tc.cache_variables["double-conversion_ROOT"] = dcroot
+
         deps.set_property("theora::theora", "cmake_target_name", "THEORA::THEORA")
         if self.settings.os != "Windows" or self.settings.os.subsystem in ["msys", "msys2"]:
             deps.set_property("theora::theoradec", "cmake_target_name", "THEORA::DEC")
             deps.set_property("theora::theoraenc", "cmake_target_name", "THEORA::ENC")
 
+        tc.generate()            
         deps.generate()
 
     def build(self):
